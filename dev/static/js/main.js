084518d3195d5620af15a448d1799e193510f84c
acubick <acubick@gmail.com>
2021-08-06 15:41:32 +0300

    Finishh

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,101 @@
+$(document).ready(function() {
+    // var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    // console.log(is_safari);
 
-// document.body.style.background = 'blue';
-$($ => {
-  console.log($, 'Hello')
-})
+    let headerH = $('.header').height(),
+        scrollPos = $(window).scrollTop(),
+        navH = $('.navigation').innerHeight();
+
+    checkScroll(scrollPos, headerH);
+    $(document).on('scroll', onScroll);
+
+    /* Fixed header when scroll
+====================================*/
+
+
+    $(window).on('scroll resize', function() {
+
+        // headerH   = headerH.height();
+        scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, headerH);
+
+    });
+
+    function checkScroll(scrollPos, headerH) {
+        if (scrollPos > headerH) {
+            $('.navigation').addClass('fixed');
+
+            $('.header').css({
+                'paddingTop': navH
+            });
+        } else {
+            $('.navigation').removeClass('fixed');
+            $('.header').removeAttr('style');
+        }
+    }
+
+    // анимация перехода по id
+    $('#nav a').on('click', function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор блока с атрибута href
+        let id = $(this).attr('href'),
+            scrollPos = $(document).scrollTop(),
+            lenth = $(id).offset().top - (navH - 55),
+            distance = lenth - scrollPos,
+            time = 300,
+            count = Math.abs(distance / 1000);
+
+        //  console.log('lenth :', lenth );
+        // console.log('scrollPos:', scrollPos);
+        // console.log( 'distance / 1000: ', distance / 1000 );
+
+        time *= parseInt(count);
+        if (time < 500) {
+            time += 700;
+        }
+
+        // console.log( 'count: ', parseInt(count) );
+        // console.log( 'time: ', time );
+        //анимируем переход на расстояние - top за 1000 мс
+        $('body,html').animate({ scrollTop: lenth }, time);
+        $('.nav-toggle, #nav').removeClass('active');
+    });
+
+
+    /* Slider */
+
+    $('.slider__slick').slick();
+
+
+    /* Menu nav toggle burger*/
+
+    $('#nav_toggle').on('click', function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+        $('#nav').toggleClass('active');
+    });
+
+
+    // подсветка пункта меню, в зависимости от текущего местоположения на странице
+    $(document).on('scroll', onScroll);
+
+    function onScroll(event) {
+        let scrollPos = $(document).scrollTop();
+        $('#nav a').each(function() {
+            let currLink = $(this),
+                refElement = $(currLink.attr('href'));
+            if (refElement.position().top <= (
+                    scrollPos + navH
+                ) && refElement.position().top + refElement.height() > (
+                    scrollPos + navH
+                )) {
+                $('#nav ul li a').removeClass('active');
+                currLink.addClass('active');
+            } else {
+                currLink.removeClass('active');
+            }
+        });
+    }
+});
